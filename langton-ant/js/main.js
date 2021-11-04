4f25e552e354b59dadc7a020c92068b87c9578ab
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-11-04 09:39:08 +0100

    langton ant, suite

--- langton-ant/js/main.js
+++ langton-ant/js/main.js
@@ -1,3 +1,4 @@
+import { setPixel } from './utils/canvas.js'
 
 const width = 128
 const height = 128
@@ -5,3 +6,5 @@ const height = 128
 const canvas = document.querySelector('canvas')
 canvas.width = width
 canvas.height = height
+
+setPixel(10, 10, 'red')
