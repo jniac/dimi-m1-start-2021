23e0b004356e5356a57d1eaa59d01f28d40f08f5
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-11-17 16:48:41 +0100

    play/pause button!

--- langton-ant/js/main.js
+++ langton-ant/js/main.js
@@ -1,8 +1,25 @@
 import { resizeCanvas } from './utils/canvas.js'
 import * as ant from './ant.js'
 
-resizeCanvas(32, 32)
+resizeCanvas(64, 64)
+
+let paused = true
+
+const loop = () => {
+  requestAnimationFrame(loop)
+  if (paused === false) {
+    ant.move()
+  }
+}
+
+loop()
+
+// UI: 
 
 document.querySelector('button#move').onclick = () => {
   ant.move()
+}
+
+document.querySelector('button#play-pause').onclick = () => {
+  paused = !paused
 }
